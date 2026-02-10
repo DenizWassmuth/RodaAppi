d1f0436251668096a5be7a11bab758a3261a47e3
DenizWassmuth <deniz-wassmuth@gmx.de>
2026-02-10 12:10:02 +0100

    moved login to outer scope in frontend, update pom.xml flapdoodle scope = test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,28 +2,26 @@ import './App.css'
 import axios from "axios";
 import {useEffect} from "react";
 
+function login(){
+    // schaue wo sind wir gerade und passe die Zieladresse entsprechend an
+    const host:string = window.location.host === "localhost:5173" ?
+        "http://localhost:8080" : window.location.origin
+    window.open(host + "/oauth2/authorization/github", "_self")
+}
+
+const loadUser = () => {
+    axios.get("/api/auth")
+        .then((response) => {console.log(response.data)})
+}
+
 function App() {
-
-    function login(){
-        // schaue wo sind wir gerade und passe die Zieladresse entsprechend an
-        const host:string = window.location.host === "localhost:5173" ?
-            "http://localhost:8080" : window.location.origin
-        window.open(host + "/oauth2/authorization/github", "_self")
-    }
-
-    const loadUser = () => {
-        axios.get("/api/auth")
-            .then((response) => {console.log(response.data)})
-    }
 
     useEffect(() => {
         loadUser();
     }, []);
 
   return (
-    <>
-        <button onClick={login}>Login</button>
-    </>
+      <button onClick={login}>Login</button>
   )
 }
 
