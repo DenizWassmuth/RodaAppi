7f5dd5c3bb5cd1fb542e3529d54fd256be00b460
DenizWassmuth <deniz-wassmuth@gmx.de>
2026-02-12 12:29:49 +0100

    remove import

--- backend/src/main/java/org/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package org.example.backend.security;
 
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
