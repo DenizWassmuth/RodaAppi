fe19237c98281c3ce640e878bbca6004f6b0f7c5
DenizWassmuth <deniz-wassmuth@gmx.de>
2026-02-23 12:09:52 +0100

    update Frontend

--- frontend/src/components/pages/EditCapoEventPage.tsx
+++ frontend/src/components/pages/EditCapoEventPage.tsx
@@ -42,7 +42,7 @@ export default function EditCapoEventPage(props:Readonly<Props>) {
     };
 
     async function submit(value: EventFormValue) {
-        if (!props.user?.id){
+        if (!props.user){
             throw new Error("Not logged in");
         }
 
